6f0d56bc1ce180fff534db47ece9456c7a90ca98
OmarAliSaid <omar.ali@stud.fci-cu.edu.eg>
2015-10-16 13:32:31 -0700

    Update Constants.java

--- app/src/main/java/com/google/project/Utilites/Constants.java
+++ app/src/main/java/com/google/project/Utilites/Constants.java
@@ -4,13 +4,8 @@ package com.google.project.Utilites;
  * Created by OmarAli on 02/10/2015.
  */
 public class Constants {
-
-<<<<<<< HEAD
     public static String API_KRY="put your api key";
-=======
     public static String API="https://api.themoviedb.org/3";
-    public static String API_KRY="ffa7755e3296424fd59fe9178bf8edbe";
     public static String YOUTUBE_URL="https://www.youtube.com/watch?v=";
->>>>>>> new structure
     public static String BASE_URL="http://image.tmdb.org/t/p/w185/";
 }
